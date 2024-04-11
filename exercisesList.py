070bc90c9f8db3ebf982147203d11379d65665a5
dj3425 <166674645+dj3425@users.noreply.github.com>
2024-04-10 20:08:28 -0600

    Add files via upload

---
+++ exercisesList.py
@@ -0,0 +1,13 @@
+list = [1, 2, 3, 4]
+list.append(5)
+print(list)
+del list[4]
+print(list)
+list.insert(2,2.5)
+print(list)
+list2 = [8, 7, 6, 1.5]
+list += list2
+print(list)
+list.sort()
+print(list)
+
